1573b94859b309cf8b74ea83400d977ead305d85
Hy-LeTuan <letuanhy1507@gmail.com>
2025-03-30 22:29:20 -0500

    read shader with string stream

--- src/shaders/shaders.cpp
+++ src/shaders/shaders.cpp
@@ -1,12 +1,14 @@
 #include <shader_s.h>
 
+#include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 namespace Shader {
 Shader::Shader(const char *vertexPath, const char *fragmentPath) {
-    std::string vertexShaderCode;
-    std::string fragmentShaderCode;
+    std::string vertexCode;
+    std::string fragmentCode;
 
     std::ifstream vertexShaderFile;
     std::ifstream fragmentShaderFile;
@@ -14,6 +16,31 @@ Shader::Shader(const char *vertexPath, const char *fragmentPath) {
     vertexShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     fragmentShaderFile.exceptions(std::ifstream::failbit |
                                   std::ifstream::badbit);
+    try {
+        vertexShaderFile.open(vertexPath);
+        fragmentShaderFile.open(fragmentPath);
+
+        std::stringstream vertexShaderStream, fragmentShaderStream;
+
+        // read file content into stream
+        vertexShaderStream << vertexShaderFile.rdbuf();
+        fragmentShaderStream << fragmentShaderFile.rdbuf();
+
+        // close stream
+        vertexShaderFile.close();
+        fragmentShaderFile.close();
+
+        // load string stream into string
+        vertexCode = vertexShaderStream.str();
+        fragmentCode = fragmentShaderStream.str();
+    } catch (std::ifstream::failure e) {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+    }
+
+    const char *vertexShaderCode = vertexCode.c_str();
+    const char *fragmentShaderCode = fragmentCode.c_str();
+
+    // compile the shader
 }
 
 }; // namespace Shader
